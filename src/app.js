bb4a5dd3b90684d3dac071a32ee15fef34426a75
Fatemeh <85831205+fateme726@users.noreply.github.com>
2021-07-23 15:12:29 +0430

    updated the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemprature(response) {
   let tempratureElement = document.querySelector("#temprature");
   tempratureElement.innerHTML = Math.round(response.data.main.temp);
@@ -9,6 +32,8 @@ function displayTemprature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = response.data.wind.speed;
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "b6d73f272b32c7eb8715d70996cfa1dc";
